1c9d377c12d936e95c5e5b440765a67d04abe5fa
Nizzo07 <afonso7557@gmail.com>
2023-06-19 20:44:15 +0100

    Counter

---
+++ Counter/app.js
@@ -0,0 +1,29 @@
+// set inicial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else if (styles.contains("add-10")) {
+            count += 10;
+        } else if (styles.contains("add-0.5")) {
+            count += 0.5;
+        } else if (styles.contains("divideBy2")) {
+            count /= 2;
+        } else {
+            count = 0;
+        }
+
+        value.textContent = count;
+    });
+});
